918100193b37d3773092588e20a9f6425c6a3952
Joao Vitor <jaoo.motta@gmail.com>
2018-06-10 02:08:21 -0300

    GO

--- src/app/app/components/card/card.component.ts
+++ src/app/app/components/card/card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PeopleService } from "../../services/people.service";
+import { GoogleImageService } from '../../services/google-image.service';
 
 @Component({
   selector: 'card',
@@ -11,18 +12,28 @@ export class CardComponent implements OnInit {
   peopleList:any[];
   firstRow:any[];
   secondRow:any[];
+  pictureList:any[];
   
-  constructor(private peopleService:PeopleService) { }
+  constructor( private peopleService:PeopleService, private googleService:GoogleImageService ) { }
 
   ngOnInit() {
+    this.findAllPersons();
+  }
+
+  private findAllPersons(){
     this.peopleService.findAllPersons().subscribe((data) => {
       this.peopleList = data.results;
+      this.getPersonsImages();
       this.firstRow = this.peopleList.slice(0,5);
-
-      this.peopleService.getPicturebyName(this.peopleList);
-
       this.secondRow = this.peopleList.slice(5,10);
     });
+  }
 
+  private getPersonsImages(){
+    this.peopleList.forEach((item)=>{
+      this.googleService.getPersonImages(item.name).subscribe((data) =>{
+          item.pic = data.items[0].link;
+      });
+    });
   }
 }
